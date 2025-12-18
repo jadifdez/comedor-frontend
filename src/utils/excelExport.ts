d68c26cfa98971b3e6f5ef3316b4433566ac3381
Antonio Gámez Nieto <45465129+antoniogameznieto@users.noreply.github.com>
2025-12-18 14:34:45 +0100

    Updated excelExport.ts

--- src/utils/excelExport.ts
+++ src/utils/excelExport.ts
@@ -89,7 +89,8 @@ export function exportarFacturacionPorAlumnosAExcel({ mesSeleccionado, facturaci
   const sheetData: any[][] = [
     ['', 'Alumnos', '', '', '', 'Totales', 'COMIDAS'],
     ['', 'Nombre', 'Secciones', 'Matrícula', 'Act. y serv.', 'Totales', 'Importe'],
-    ['', '', '', '', '', '0,00', '(0)']
+    ['', '', '', '', '', '0,00', '(0)'],
+    ['', '', '', '', '', '', '02a900a1-88b6-7031-1eac-22102b3ede08']
   ];
 
   registros.forEach(reg => {
